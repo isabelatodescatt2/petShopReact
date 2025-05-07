c318092f815093e9e6724f28b80819988b6f27c9
Luis-Tanese <luistanese@gmail.com>
2025-05-07 11:22:12 -0300

    womp

--- src/pages/NavbarMenu.js
+++ src/pages/NavbarMenu.js
@@ -2,12 +2,15 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import petShopLogo from "../imagens/petShopLogo.png";
 
 const NavbarMenu = () => {
 	return (
 		<Navbar bg="light" data-bs-theme="light">
 			<Container>
-				<Navbar.Brand href="#home">Navbar</Navbar.Brand>
+				<Navbar.Brand href="#home">
+					<img src={petShopLogo} alt="logo"></img>
+				</Navbar.Brand>
 				<Nav className="me-auto">
 					<Nav.Link href="#home">Home</Nav.Link>
 					<Nav.Link href="#features">Features</Nav.Link>
